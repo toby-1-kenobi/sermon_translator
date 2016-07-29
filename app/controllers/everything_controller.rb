4e431c9c49c601d7c6b939f2b3609f764e13bb8e
Toby Anderson <toby.anderson@gmail.com>
2016-07-29 11:42:21 +1000

    Generate a new rails project with one 'everything' controller that submits text to Google for translation after chunking it.

---
+++ app/controllers/everything_controller.rb
@@ -0,0 +1,47 @@
+require 'uri'
+
+class EverythingController < ApplicationController
+
+  def home
+
+  end
+
+  def translation
+    logger.debug "API KEY #{Rails.application.secrets.G_TRANSLATE_KEY}"
+    sermon_chunks = chunk_text_by_sentence(params[:sermon], 3000)
+    g_translate_url = 'https://www.googleapis.com/language/translate/v2'
+    g_translate_source = 'en'
+    g_translate_target = 'de'
+    @translation = ''
+    sermon_chunks.each do |chunk|
+      response = HTTParty.get(
+          "#{g_translate_url}?" +
+              "key=#{Rails.application.secrets.G_TRANSLATE_KEY}" +
+              "&source=#{g_translate_source}" +
+              "&target=#{g_translate_target}" +
+              "&q=#{URI.encode sermon_chunks[0]}"
+      )
+      @translation += ' ' + response['data']['translations'][0]['translatedText']
+    end
+  end
+
+  private
+
+  def chunk_text_by_sentence(text, char_limit)
+    sentences = text.split('.')
+    chunks = Array.new
+    chunk = ''
+    sentences.each do |sentence|
+      if chunk.size + sentence.size > char_limit
+        chunks << chunk
+        chunk = ''
+      end
+      chunk += sentence
+    end
+    if chunk.present?
+      chunks << chunk
+    end
+    return chunks
+  end
+
+end
---
+++ app/helpers/everything_helper.rb
@@ -0,0 +1,2 @@
+module EverythingHelper
+end
